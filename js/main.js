1dbe63a0e64f18d9af63f9fe59950a0bb4601abe
linkswapnil <Shagun123>
2017-01-15 10:08:15 +0530

    handle odd even ids

--- js/main.js
+++ js/main.js
@@ -26,10 +26,38 @@
     function populateData() {
         $.when(getData()).then(function (data) {
             serviceData = data;
+            data.forEach(function (record, index) {
+                var optionId = document.createElement('option');
+                optionId.setAttribute('key', index);
+                optionId.value = record.id;
+                optionId.text = record.id;
+                idSelect.append($(optionId));
+
+                var title = document.createElement('option');
+                title.setAttribute('key', index);
+                title.value = record.id;
+                title.text = record.title;
+                titleSelect.append($(title));
+            });
         }, function handleFailure(res) {
             console.log(res);
         })
     }
+
+    idSelect.on('change', function (evt) {
+        var selectedOption = evt.target.options[evt.target.selectedIndex];
+        var key = +selectedOption.getAttribute('key');
+        if (+selectedOption.value % 2 === 0) {
+            titleSelect.find("option[key=" + key + "]").attr('selected', true);
+            bodyText.val(serviceData[key].body);
+            title.show();
+            body.show();
+        } else {
+            titleSelect.find("option[key=" + key + "]").attr('selected', true);
+            title.show();
+            body.hide();
+        }
+    });
 
     populateData();
 
